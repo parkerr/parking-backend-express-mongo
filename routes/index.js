cf3f9137e1774ba7d2e29e295f67cdca32628626
Richard Parker <rlparker.uk@gmail.com>
2016-10-30 15:03:07 +0000

    Add spaces routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var ctrlSpaces = require('../controllers/spaces');
+
+//spaces
+router.get('/spaces', ctrlSpaces.getAllSpaces);
+router.get('/spaces/:spaceid', ctrlSpaces.getOneSpace);
+router.put('/spaces/:spaceid', ctrlSpaces.updateOneSpace);
 
 module.exports = router;
