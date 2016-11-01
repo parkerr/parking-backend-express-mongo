60d18c07d9c8a4a810b49a30121aa32e9d92c8a4
Richard Parker <rlparker.uk@gmail.com>
2016-11-01 08:24:37 +0000

    Amend mogolab_uri to mongodb_uri

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ var gracefulShutdown;
 
 //var dbURI = 'mongodb://localhost/parking';
 //if(process.env.NODE_ENV == 'production'){
-  dbURI = process.env.MONGOLAB_URI;
+  dbURI = process.env.MONGODB_URI;
 //}
 mongoose.connect(dbURI);
 
