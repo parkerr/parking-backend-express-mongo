9f5a99eb2db878144074634ae4eb52afec37c0a0
Richard Parker <rlparker.uk@gmail.com>
2016-11-11 16:32:07 +0000

    Tidy Up Controller

--- controllers/spaces.js
+++ controllers/spaces.js
@@ -1,50 +1,34 @@
 var mongoose = require('mongoose');
 var spaces = mongoose.model('Space');
 
-
-
-
+:
 var getAll = function(req, res){
-
   spaces.find().sort({number: 1}).exec(function(err, space){
-    
     if(!space.length){
-      
       sendJsonResponse(res, 404, {"message" : "no spaces found"});
       return
-      
     } else if(err){
-      
       sendJsonResponse(res, 404, err);
       return
     }
     sendJsonResponse(res, 200, space);
   });
-  
 };
-
 
 
 module.exports.getAllSpaces = function(req, res){
   getAll(req, res);
 };
 
-
-
-
 module.exports.getOneSpace = function(req, res) {
   if (req.params && req.params.spacenumber){
-    
     spaces.find({number : req.params.spacenumber}).exec(function(err, space){
       if(!space.length){
-        
-        sendJsonResponse(res, 404, {"message" : "space number not found"});
-        return
-        
-      } else if(err){
-        
-        sendJsonResponse(res, 404, err);
-        return
+		  sendJsonResponse(res, 404, {"message" : "space number not found"});
+          return
+	  } else if(err){
+          sendJsonResponse(res, 404, err);
+          return
       }
       sendJsonResponse(res, 200, space);
     });
@@ -54,48 +38,28 @@ module.exports.getOneSpace = function(req, res) {
 };
 
 module.exports.makeSpaceUnavailable = function(req, res){
-spaces.findOneAndUpdate({number: req.params.spacenumber}, {$pull: {availableOn: req.params.date}}, {new: true}, function(err, space){
-          //sendJsonResponse(res, 200, space);
+    spaces.findOneAndUpdate({number: req.params.spacenumber}, {$pull: {availableOn: req.params.date}}, {new: true}, function(err, space){
 	  getAll(req, res);
-});
+    });
 };
 
 module.exports.makeSpaceAvailable = function(req, res){
-spaces.findOneAndUpdate({number: req.params.spacenumber}, {$push: {availableOn: req.params.date}}, {new: true}, function(err, space){
-          //sendJsonResponse(res, 200, space);
+    spaces.findOneAndUpdate({number: req.params.spacenumber}, {$push: {availableOn: req.params.date}}, {new: true}, function(err, space){
 	  getAll(req, res);
-});
+    });
 };
 
-module.exports.updateOneSpace = function(req, res) {};
 
+module.exports.updateOneSpace = function(req, res) {
+	//TODO
+};
 
 
 module.exports.createSpace = function(req, res){
-spaces.create({number: 1}, function(err, card){
-sendJsonResponse(res, 200, card);
-});
-
+  spaces.create({number: 1}, function(err, card){
+    sendJsonResponse(res, 200, card);
+  });
 };
-
-
-
-module.exports.createNextSpace = function(req, res){
- spaces.findOne().sort('-number').exec(function(err, item){
-   var maxnumber = item.number + 1;
-  spaces.create({
-	                 number: maxnumber
-				                                                                     }, function(err, card){
-													     if(err){
-														         sendJsonResponse(res, 404, err);
-															   } else {sendJsonResponse(res, 200, card);
-															   };
-												     }
-	       )
-
-
- });};
-
 
 
 var sendJsonResponse = function(res, status, content){
